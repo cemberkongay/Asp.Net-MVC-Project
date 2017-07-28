461f1a99b823923925e3ba47f83697afe82d7163
Cem Berk Öngay <cberkongay@gmail.com>
2017-07-29 02:28:43 +0300

    bootbox, dataTable and jquery

--- Vidly/Dtos/CustomerDto.cs
+++ Vidly/Dtos/CustomerDto.cs
@@ -20,6 +20,8 @@ namespace Vidly.Dtos
         [Display(Name = "Membership Type")]
         public byte MembershipTypeId { get; set; }
 
+        public MembershipType MembershipType { get; set; }
+
         [Display(Name = "Date of Birth")]
         //[Min18YearsIfAMember]
         public DateTime? Birthdate { get; set; }
